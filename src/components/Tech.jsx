7da32d9462f69b624fb2660b04af4a3832d998eb
Breno <you@example.com>
2023-11-12 15:59:40 -0300

    2.0

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,22 +1,17 @@
 import React from "react";
-import { useMediaQuery } from "react-responsive";
+
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
-  const isMobile = useMediaQuery({ maxWidth: 768 }); // Adjust the maxWidth as needed
-
   return (
-    <div className='text-center'>
-      <h1 className='text-2xl font-bold mb-4'>HABILIDADES</h1>
-      <div className={`flex flex-row flex-wrap justify-center gap-10 ${isMobile ? 'mobile-view' : ''}`}>
-        {technologies.map((technology) => (
-          <div className={`w-28 h-28 ${isMobile ? 'mobile-ball' : ''}`} key={technology.name}>
-            <BallCanvas icon={technology.icon} />
-          </div>
-        ))}
-      </div>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
     </div>
   );
 };
