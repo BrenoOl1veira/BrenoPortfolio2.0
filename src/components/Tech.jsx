b930be7d33a52b3035d515129f9e5209e89ba543
Breno <you@example.com>
2023-11-12 16:25:36 -0300

    2.4

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,14 +6,17 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
+    <div className='text-center'>
+      <h1 className='text-2xl font-bold mb-4'>Habilidades</h1>
+      <div className='flex flex-row flex-wrap justify-center gap-10'>
+        {technologies.map((technology) => (
+          <div className='w-28 h-28' key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
     </div>
-  );
-};
+  )
+}
 
 export default SectionWrapper(Tech, "");
